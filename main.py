26731e5f4fd24b988a9c408bc8d3360f18072cb0
yakimenko <r.yakimenko.73@gmail.com>
2021-03-30 14:39:07 +0300

    add cyclic visits to profiles

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
 driver.get("http://facebook.com")
@@ -12,17 +14,30 @@ driver.find_element_by_id("pass").send_keys("cxzdsaewq321")  # input поле п
 driver.find_element_by_xpath("//*[contains(@id,'u_0_c_')]").click()
 driver.find_element_by_xpath("//*[contains(@id,'u_0_d_')]").click()
 
-element = driver.find_element_by_xpath("//*[contains(@id,'mount_0_0_')]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[3]/div/div/div[1]/div/div[2]/div/div[2]/div/ul")
-friends = element.find_elements_by_tag_name("li")
 
-links_to_friends = []
 for i in range(10):
-	friends[i].click()
-	time.sleep(2)
-	driver.find_element_by_xpath("//*[contains(@id,'mount_0_0_')]/div/div[1]/div/div[5]/div/div[1]/div[1]/div[1]/div/div/div/div/div/div/div[1]/div/div/div[1]/span/div/div[1]").click()
-	time.sleep(1)
-	button_view_profile = driver.find_element_by_xpath("//*[contains(@id,'mount_0_0_')]/div/div[1]/div/div[5]/div/div[1]/div[1]/div[2]/div/div/div[1]/div[1]/div/div/div[1]/div/div[1]/div/a[2]")
-	links_to_friends.append(button_view_profile.get_attribute("href"))
-	driver.find_element_by_xpath("//*[contains(@id,'mount_0_0_')]/div/div[1]/div/div[5]/div/div[1]/div[1]/div[1]/div/div/div/div/div/div/div[1]/div/div/div[3]/span[4]/div").click()
+	print(i)
 
-print(links_to_friends)
+	# get friends list
+	contacts = WebDriverWait(driver, 10).until(
+		EC.presence_of_element_located((By.XPATH, "//*[contains(@id,'mount_0_0_')]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[3]/div/div/div[1]/div/div[2]/div/div[2]/div/ul")))
+	time.sleep(3)
+	friends = contacts.find_elements_by_tag_name("li")
+
+	friends[i].click()
+
+	# click button friend info
+	element = WebDriverWait(driver, 10).until(
+		EC.presence_of_element_located((By.XPATH, "//*[contains(@id,'mount_0_0_')]/div/div[1]/div/div[5]/div/div[1]/div[1]/div[1]/div/div/div/div/div/div/div[1]/div/div/div[1]/span/div/div[1]")))
+	element.click()
+
+	button_view_profile = WebDriverWait(driver, 10).until(
+		EC.presence_of_element_located((By.XPATH, "//*[contains(@id,'mount_0_0_')]/div/div[1]/div/div[5]/div/div[1]/div[1]/div[2]/div/div/div[1]/div[1]/div/div/div[1]/div/div[1]/div/a[2]")))
+	time.sleep(3)
+	driver.get(button_view_profile.get_attribute("href"))
+
+	# remove profile frame
+	button_view_profile = WebDriverWait(driver, 10).until(
+		EC.presence_of_element_located((By.XPATH, "//*[contains(@id,'mount_0_0_')]/div/div[1]/div/div[5]/div/div[1]/div[1]/div[1]/div/div/div/div/div/div/div[1]/div/div/div[3]/span[4]/div")))
+
+	driver.back()
