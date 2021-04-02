3d72a696ddd8495e407a1be617c9872da24b352b
yakimenko <r.yakimenko.73@gmail.com>
2021-04-02 20:48:55 +0300

    add config and logger

---
+++ constants.py
@@ -0,0 +1,45 @@
+# list of correct log levels
+TRUE_LOG_LEVELS = [
+	'critical', 
+	'error', 
+	'warning', 
+	'info', 
+	'debug', 
+]
+
+# list of correct file modes
+TRUE_FILE_MODES = [
+	'r',
+	'w',
+	'x',
+	'a',
+	'b',
+	't',
+	'+',
+]
+
+FRIEND_DATA_ATTRIBUTES = [
+	'NAME',
+	'URL',
+]
+
+DATE_FORMAT_FOR_LOGGER = '%Y-%m-%d %H:%M:%S'
+
+MESSAGE_FORMAT_FOR_LOGGER = '%(asctime)s.%(msecs)03d %(name)s %(levelname)s: %(message)s'
+
+# format for displaying the list of friends in the console
+FORMAT_DISPLAYING_FRIENDS = '{:<25}{:<25}'
+
+MY_PROFILE_XPATH = "//*[contains(@id,'mount_0_0_')]/div/div[1]/div/div[2]/div[4]/div[1]/div[4]"
+
+COUNTER_FRIENDS_XPATH = "//*[contains(@id,'mount_0_0_')]/div/div[1]/div/div[3]/div/div/div[2]/div[1]/div/div/div[4]/div[2]/div/div[1]/div[2]/div/div[3]/div/div/div/div/div[1]/div/div/div/div[2]/span/span"
+
+FRIEND_LIST_DOM_XPATH = "//*[contains(@id,'mount_0_0_')]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[3]"
+
+FRIEND_UPLOAD_TAG_CLASSNAME = "rek2kq2y"
+
+FRIEND_CLASSNAME = "//*[contains(@class, 'bp9cbjyn ue3kfks5 pw54ja7n uo3d90p7 l82x9zwi n1f8r23x rq0escxv j83agx80 bi6gxh9e discj3wi hv4rvrfc ihqw7lf3 dati1w0a gfomwglr')]"
+
+SHOW_PASSWORD_BUTTON_XPATH = "//*[contains(@id,'u_0_c_')]"
+
+LOG_IN_BUTTON_XPATH = "//*[contains(@id,'u_0_d_')]"
--- main.py
+++ main.py
@@ -1,43 +1,205 @@
-import time
+import os
+import re
+import csv
+import datetime as dt
+
+import logging
+import configparser
+
+import selenium
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-driver.get("http://facebook.com")
-
-driver.find_element_by_id("email").send_keys("0680656829")  # input поле логина
-driver.find_element_by_id("pass").send_keys("cxzdsaewq321")  # input поле пароля 
-driver.find_element_by_xpath("//*[contains(@id,'u_0_c_')]").click()
-driver.find_element_by_xpath("//*[contains(@id,'u_0_d_')]").click()
+from constants import *
+from settings import secret
 
 
-for i in range(10):
-	print(i)
+def setup():
+	parameters_set = config_setup()
+	logging_setup(
+		parameters_set["Path"]["path_to_log"], 
+		*parameters_set["LoggingSettings"].values(),
+	)
 
-	# get friends list
-	contacts = WebDriverWait(driver, 10).until(
-		EC.presence_of_element_located((By.XPATH, "//*[contains(@id,'mount_0_0_')]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[3]/div/div/div[1]/div/div[2]/div/div[2]/div/ul")))
-	time.sleep(3)
-	friends = contacts.find_elements_by_tag_name("li")
+	return parameters_set
 
-	friends[i].click()
 
-	# click button friend info
-	element = WebDriverWait(driver, 10).until(
-		EC.presence_of_element_located((By.XPATH, "//*[contains(@id,'mount_0_0_')]/div/div[1]/div/div[5]/div/div[1]/div[1]/div[1]/div/div/div/div/div/div/div[1]/div/div/div[1]/span/div/div[1]")))
-	element.click()
+def config_setup():
+	path_to_config = 'settings/config.ini'
+	config = configparser.ConfigParser()
+	config.read(path_to_config)
 
-	button_view_profile = WebDriverWait(driver, 10).until(
-		EC.presence_of_element_located((By.XPATH, "//*[contains(@id,'mount_0_0_')]/div/div[1]/div/div[5]/div/div[1]/div[1]/div[2]/div/div/div[1]/div[1]/div/div/div[1]/div/div[1]/div/a[2]")))
-	time.sleep(3)
-	driver.get(button_view_profile.get_attribute("href"))
+	parameters_set = {}
 
-	# remove profile frame
-	button_view_profile = WebDriverWait(driver, 10).until(
-		EC.presence_of_element_located((By.XPATH, "//*[contains(@id,'mount_0_0_')]/div/div[1]/div/div[5]/div/div[1]/div[1]/div[1]/div/div/div/div/div/div/div[1]/div/div/div[3]/span[4]/div")))
+	try:
+		for section in config:
+			if section != "DEFAULT":
+				parameters_set[section] = {}
+			for field in config[section]:
+				if section == "LoggingSettings":
+					parameters_set[section][field] = config[section][field].lower()
+				elif section == "Path":
+					parameters_set[section][field] = config[section][field]
+				else:
+					parameters_set[section][field] = int(config[section][field])
+		if not parameters_set:
+			raise FileNotFoundError("Config file not found")
+	except (ValueError, FileNotFoundError, ) as ex:
+		print("Incorrect parameters in the config file or the file is missing at all. " +
+			f"Path: {path_to_config}. Ex: {ex}")
 
-	driver.back()
+		os._exit(0)
+
+	return parameters_set
+
+
+def logging_setup(path_to_log, log_level, log_filemode):
+	if not log_level in TRUE_LOG_LEVELS:
+		log_level = 'DEBUG'
+
+	if not log_filemode in TRUE_FILE_MODES:
+		log_filemode = 'a'
+
+	path = create_file_path(path_to_log)
+
+	logging.basicConfig(filename=path, 
+		level=log_level,
+		filemode=log_filemode, 
+		format=MESSAGE_FORMAT_FOR_LOGGER,
+		datefmt=DATE_FORMAT_FOR_LOGGER)
+
+
+def workflow(parameters):
+	driver = webdriver.Chrome(parameters["Path"]["path_to_driver"])
+	driver.maximize_window()
+	
+	list_friends = parse_friends(driver)
+
+	try:
+		path_to_csv = parameters["Path"]["path_to_csv"]
+	except KeyError as ex:
+		logging.warning("The path to the csv file is missing in the config. Creating a file in the executing directory.")
+		path_to_csv = "orders.csv"
+
+	path = create_file_path(path_to_csv)
+
+	logging.debug(f"An attempt to write a list of friends to a csv file. Path: {path}.")
+	write_csv(path, list_friends)
+
+	logging.debug(f"An attempt to read a list of friends from a csv file. Path: {path}.")
+	read_csv(path)
+
+
+def log_in_facebook(driver):
+	driver.get("http://facebook.com")
+
+	driver.find_element_by_id("email").send_keys(secret.login)
+	driver.find_element_by_id("pass").send_keys(secret.password)
+
+	driver.find_element_by_xpath(SHOW_PASSWORD_BUTTON_XPATH).click()
+	driver.find_element_by_xpath(LOG_IN_BUTTON_XPATH).click()
+
+
+def parse_friends(driver):
+	list_friends = []
+	log_in_facebook(driver)
+	go_to_my_profile(driver)
+	number_friends = get_number_friends(driver)
+	
+	driver.get(f'{driver.current_url}&sk=friends')
+
+	list_DOM = get_friend_list_DOM(driver)
+	for i in range(number_friends):
+		friends = list_DOM.find_elements_by_xpath(FRIEND_CLASSNAME)
+
+		friend_data = {
+			"name": friends[i].find_element_by_xpath("div[2]/div[1]").text,
+			"url": friends[i].find_element_by_xpath("div/a").get_attribute("href")
+		}
+		
+		list_friends.append(friend_data)
+
+	return list_friends
+
+
+def go_to_my_profile(driver):
+	button_my_profile = WebDriverWait(driver, 10).until(
+	EC.presence_of_element_located((By.XPATH, MY_PROFILE_XPATH)))
+	button_my_profile.click()
+
+
+def get_number_friends(driver):
+	number_friends_dom = WebDriverWait(driver, 10).until(
+	EC.presence_of_element_located((By.XPATH, COUNTER_FRIENDS_XPATH)))
+
+	actions = ActionChains(driver)
+	actions.move_to_element(number_friends_dom).perform()
+
+	number_friends = int(number_friends_dom.text.split(": ")[1])
+
+	return number_friends
+
+
+def get_friend_list_DOM(driver):
+	list_DOM = WebDriverWait(driver, 10).until(
+		EC.presence_of_element_located((By.XPATH, FRIEND_LIST_DOM_XPATH)))
+	flag = True
+	while flag:
+		try:
+			upload_tag = list_DOM.find_element_by_class_name(FRIEND_UPLOAD_TAG_CLASSNAME)
+			actions = ActionChains(driver)
+			actions.move_to_element(upload_tag).perform()
+		except selenium.common.exceptions.NoSuchElementException:
+			flag = False
+
+	return list_DOM
+
+
+def create_file_path(path):
+	pathdir = ''.join(re.findall(r"\w+/", path))
+	if pathdir:
+		if not os.path.exists(pathdir):
+			try:
+				os.makedirs(pathdir)
+			except OSError as ex:
+				logging.warning("Failed to create file in the selected path. " +
+					f"Created a file in the executing directory. Path: {path}. Ex: {ex}")
+				path = os.path.basename(path)
+	return path
+
+
+def write_csv(filename, list_friends):
+	try:
+		with open(filename, "w") as f:
+			csv_f = csv.writer(f)
+			csv_f.writerow(FRIEND_DATA_ATTRIBUTES)
+
+			for friend in list_friends:
+				csv_f.writerow(friend.values())
+	except OSError as ex:
+		logging.error(f"Failed to write data to file. Path: {filename}. Ex: {ex}")
+		return 0
+
+
+def read_csv(filename):
+	try:
+		with open(filename, "r", newline="") as f:
+			csv_f = csv.reader(f)
+
+			for row in csv_f:
+				if row:
+					print(FORMAT_DISPLAYING_FRIENDS.format(*row))
+	except OSError as ex:
+		logging.error(f"Failed to read data from file. Path: {filename}. Ex: {ex}")
+		return 0
+
+
+if __name__ == "__main__":
+	parameters_set = setup()
+	logging.debug("Config and logger setup was successful. " + 
+		f"Number of sections from config: {len(parameters_set.keys())}")
+	workflow(parameters_set)
